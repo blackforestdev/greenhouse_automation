ea2b5de931032f0605b2e91371aac347e0d56ddc
logan hedin <6.28x10e18@gmail.com>
2023-11-06 07:40:22 +0000

    Update file domManager.js

--- static/js/domManager.js
+++ static/js/domManager.js
@@ -1,81 +1,50 @@
 import { getCurrentTime } from './timeManager.js';
 
-// Initialize the socket connection
 const socket = io();
 
-// Update the content of the #current-time element with the current time
 export function updateCurrentTimeElement() {
     const currentTimeElement = document.querySelector('#current-time');
     currentTimeElement.textContent = getCurrentTime();
 }
 
-// Initialize time updating: set the current time immediately and then update every 1 second
 export function initTimeUpdater() {
-    updateCurrentTimeElement();  // Initial update
-    setInterval(updateCurrentTimeElement, 1000);  // Update every 1 second
+    updateCurrentTimeElement();
+    setInterval(updateCurrentTimeElement, 1000);
 }
 
-export function triggerMotor(action) {
+export function triggerMotor(action, motorId) {
     fetch(`/motor_action/${action}`, {
-        method: 'POST'
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ motor_id: motorId })
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        alert(`${action.replace('_', ' ')} successfully triggered!`);
-    })
+    .then(response => response.json())
+    .then(data => alert(`${action.replace('_', ' ')}: ${data.message}`))
     .catch(error => {
-        console.error('There was a problem with the fetch operation:', error.message);
+        console.error('Fetch operation error:', error.message);
         alert('An error occurred.');
     });
 }
 
-// Function to handle the motor control and stop motor button events
 function motorControlButtonListener() {
     document.querySelectorAll('.motor-control-btn').forEach(button => {
         button.addEventListener('click', () => {
             const action = button.dataset.action;
-            if (action === 'stop_motors') {
-                socket.emit('stop_motor');
-            } else {
-                triggerMotor(action);
-            }
+            const motorId = button.dataset.motorId; // Assuming motorId is added to dataset
+            triggerMotor(action, motorId);
         });
     });
 }
 
-// Function to request roll up and down times from the server
 export function requestTimes() {
     socket.emit('request_current_times');
 }
 
-// Event listeners for socket events
-socket.on('current_times', (data) => {
+socket.on('current_times', data => {
     document.getElementById('roll-up-time').textContent = data.roll_up || "Not set";
     document.getElementById('roll-down-time').textContent = data.roll_down || "Not set";
 });
 
-socket.on('motor_action_result', (data) => {
-    if (data.action && data.result) {
-        alert(`${data.action.replace('_', ' ')} ${data.result}`);
-    } else {
-        alert('An error occurred with the motor action.');
-    }
-});
-
-socket.on('stop_motor_result', (data) => {
-    if (data.status === 'success') {
-        alert('Motors stopped successfully!');
-    } else {
-        alert('Failed to stop motors.');
-    }
-});
-
-// Switches to Activate or Deactivate each motor
 export function initMotorSwitches() {
     const motorSwitches = [
         { id: 'sidewall-left-switch', statusElem: 'sidewall-left-status' },
@@ -86,111 +55,83 @@ export function initMotorSwitches() {
 
     motorSwitches.forEach(motor => {
         const switchElem = document.getElementById(motor.id);
-        switchElem.addEventListener('change', function() {
+        switchElem.addEventListener('change', () => {
             const status = switchElem.checked ? 'Active' : 'Deactivated';
             document.getElementById(motor.statusElem).textContent = status;
-
-            // Now, send the status to the server
-            fetch(`/motor_status/${motor.id}`, {
-                method: 'POST',
-                body: JSON.stringify({ status: status }),
-                headers: { 'Content-Type': 'application/json' }
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error.message);
-            });
+            updateMotorStatus(motor.id, status);
         });
     });
+}
+
+function updateMotorStatus(motorId, status) {
+    fetch(`/motor_status/${motorId}`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ status })
+    })
+    .catch(error => console.error('Error updating motor status:', error));
 }
 
 export function fetchMotorStatuses() {
     fetch('/get_motor_statuses')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        data.forEach(motor => {
-            let motorName;
-            switch (motor.motor_id) {
-                case 1: motorName = 'sidewall-left'; break;
-                case 2: motorName = 'sidewall-right'; break;
-                case 3: motorName = 'overhead-left'; break;
-                case 4: motorName = 'overhead-right'; break;
-            }
+    .then(response => response.json())
+    .then(updateMotorSwitchesUI)
+    .catch(error => console.error('Error fetching motor statuses:', error));
+}
 
-            const switchElemId = motorName + '-switch';
-            const switchElem = document.getElementById(switchElemId);
-            const statusElem = document.getElementById(motorName + '-status');
-            if (switchElem && statusElem) {
-                switchElem.checked = motor.status === 'Active';
-                statusElem.textContent = motor.status;
-            }
-        });
-    })
-    .catch(error => {
-        console.error('Error fetching motor statuses:', error);
-        alert('Failed to fetch motor statuses.');
+function updateMotorSwitchesUI(motorStatuses) {
+    motorStatuses.forEach(motor => {
+        let motorName = getMotorName(motor.motor_id);
+        let switchElemId = `${motorName}-switch`;
+        let switchElem = document.getElementById(switchElemId);
+        let statusElem = document.getElementById(`${motorName}-status`);
+        if (switchElem && statusElem) {
+            switchElem.checked = motor.status === 'Active';
+            statusElem.textContent = motor.status;
+        }
     });
 }
 
-// Function to handle the set time form submission
+function getMotorName(motorId) {
+    switch (motorId) {
+        case 1: return 'sidewall-left';
+        case 2: return 'sidewall-right';
+        case 3: return 'overhead-left';
+        case 4: return 'overhead-right';
+        default: return 'unknown';
+    }
+}
+
 export function handleSetTimeForm() {
     const setTimeForm = document.getElementById('set-time-form');
-
-    setTimeForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the default form submission
-
-        const rollUpTime = document.getElementById('roll_up_time').value;
-        const rollDownTime = document.getElementById('roll_down_time').value;
-
-        fetch('/set_time', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: `roll_up_time=${encodeURIComponent(rollUpTime)}&roll_down_time=${encodeURIComponent(rollDownTime)}`
-        })
-        .then(response => response.json())
-        .then(data => {
-            if(data.status === 'success') {
-                alert('Times set successfully!');
-                requestTimes();
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('An error occurred while setting times');
-        });
+    setTimeForm.addEventListener('submit', event => {
+        event.preventDefault();
+        let rollUpTime = document.getElementById('roll_up_time').value;
+        let rollDownTime = document.getElementById('roll_down_time').value;
+        submitTimeSettings(rollUpTime, rollDownTime);
     });
 }
 
-// Function to fetch sensor data from the Flask backend
+function submitTimeSettings(rollUpTime, rollDownTime) {
+    fetch('/set_time', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        body: `roll_up_time=${encodeURIComponent(rollUpTime)}&roll_down_time=${encodeURIComponent(rollDownTime)}`
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.status === 'success') {
+            alert('Times set successfully!');
+            requestTimes();
+        } else {
+            alert('Error setting times.');
+        }
+    })
+    .catch(error => console.error('Error setting times:', error));
+}
+
 export function fetchSensorData() {
     fetch('/get_sensor_data')
     .then(response => response.json())
-    .then(data => {
-        document.getElementById('temperature').textContent = data.temperature || "Not available";
-        document.getElementById('humidity').textContent = data.humidity || "Not available";
-        document.getElementById('vpd').textContent = data.vpd || "Not available";
-    })
-    .catch(error => console.error('Error fetching sensor data:', error));
-}
-
-// Initialize everything when the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', function() {
-    initTimeUpdater();
-    requestTimes();
-    initMotorSwitches();
-    motorControlButtonListener();
-    handleSetTimeForm();
-    fetchSensorData();
-});
+    .then(data => updateSensorDataUI(data))
+    .catch(error => console.error('Error
