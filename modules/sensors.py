ae6f73299e16a22dbce2c3e4c5dd61efddb9a28f
logan hedin <6.28x10e18@gmail.com>
2023-11-07 01:02:39 +0000

    removed old 'def get_sensor_data()' code

--- modules/sensors.py
+++ modules/sensors.py
@@ -19,25 +19,7 @@ def get_ubibot_data():
         return None
 
 # Additional processing and error handling can be added as needed
-"""
-def fetch_sensor_data(account_key, channel_id):
-    """
-    Fetches the latest sensor data (temperature and humidity) from UbiBot.
-    """
-    url = f'https://api.ubibot.com/channels/{channel_id}/feeds.json?account_key={account_key}'
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        latest_data = data['feeds'][0] if data['feeds'] else None
-        if latest_data:
-            return {
-                'temperature': latest_data.get('field1'),  # Replace with your actual field key for temperature
-                'humidity': latest_data.get('field2')  # Replace with your actual field key for humidity
-            }
-    else:
-        raise Exception(f"Failed to fetch data: {response.status_code}")
-"""
+
 def fetch_sensor_data():
     """
     Fetches the latest sensor data (temperature and humidity) from UbiBot.
