b1718c78a35b9343b850495bd8e81d091954b0e3
logan hedin <6.28x10e18@gmail.com>
2023-11-07 03:39:39 +0000

    Fixed errors in the tool

--- tools/UbiBot_API_tool.py
+++ tools/UbiBot_API_tool.py
@@ -2,28 +2,20 @@ import requests
 import json
 
 def get_ubibot_device_channels(account_key):
-    # Add the account_key as a query parameter in the URL
     url = f'https://webapi.ubibot.com/channels?account_key={account_key}'
-    
     response = requests.get(url)
     
     if response.status_code == 200:
-        # Print the raw response to understand the structure
-        print(json.dumps(response.json(), indent=4))
-        
-        channels = response.json().get('result')
+        response_data = response.json()
+        channels = response_data.get('channels', [])
         if channels:
             print("Channels (Devices) found:")
             for channel in channels:
-                # Let's print what 'channel' actually is
-                print(channel)
-                # If 'channel' is indeed a dictionary, this will work, otherwise we will adjust
-                print(f"Channel Name: {channel.get('channel_name', 'Unknown')} - Channel ID: {channel.get('channel_id', 'Unknown')}")
+                print(f"Channel Name: {channel.get('name', 'Unknown')} - Channel ID: {channel.get('channel_id', 'Unknown')}")
         else:
             print("No channels (devices) were found associated with this account.")
     else:
         print(f"Failed to retrieve channels. Status Code: {response.status_code}, Response: {response.text}")
 
-# Replace 'your_account_key_here' with your actual UbiBot account key
 account_key = 'f23b1d6093b52054e1edadd224d5a116'
 get_ubibot_device_channels(account_key)
