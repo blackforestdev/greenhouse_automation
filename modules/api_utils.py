6a5b8f740cbf9793c793fef68c9c1cf0de11e82a
logan hedin <6.28x10e18@gmail.com>
2023-11-08 10:05:45 +0000

    added  error logging to api_utils.py

--- modules/api_utils.py
+++ modules/api_utils.py
@@ -1,5 +1,3 @@
-# /modules/api_utils.py
-
 import requests
 import json
 from datetime import datetime, timedelta
@@ -17,27 +15,43 @@ UBI_READ_API_KEY = os.getenv("UBI_READ_API_KEY")
 def generate_access_token():
     url = "https://webapi.ubibot.com/accounts/generate_access_token"
     params = {"account_key": UBI_ACCOUNT_KEY}
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
         data = response.json()
         token = data.get("token_id")
         expiry_seconds = int(data.get("expire_in_seconds", 3600))
         expiry_time = datetime.now() + timedelta(seconds=expiry_seconds)
+        print(f"Access Token: {token}, Expires in: {expiry_seconds} seconds")
         return token, expiry_time
-    else:
-        print("Access Token Generation Failed:", response.text)
-        raise Exception("Failed to generate access token")
+    except requests.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}, Response: {response.text}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
 
 def get_sensor_data(token):
     headers = {'content-type': 'application/json', 'Authorization': f'Bearer {token}'}
     url = f"https://webapi.ubibot.com/channels/{UBI_CHANNEL_ID}/feeds.json"
     params = {'api_key': UBI_READ_API_KEY}
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code == 200:
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
         sensor_data = response.json()
         latest_data = sensor_data.get('feeds', [])[0] if sensor_data.get('feeds') else {}
         temperature = latest_data.get('field1', {}).get('value')
         humidity = latest_data.get('field2', {}).get('value')
+        print(f"Retrieved Sensor Data: Temperature - {temperature}, Humidity - {humidity}")
         return {'temperature': temperature, 'humidity': humidity}
-    else:
-        raise Exception(f"Failed to get sensor data: {response.text}")
+    except requests.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}, Response: {response.text}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+
+# Testing the functions (optional)
+if __name__ == "__main__":
+    try:
+        token, _ = generate_access_token()
+        if token:
+            get_sensor_data(token)
+    except Exception as e:
+        print(f"Error in main: {e}")
