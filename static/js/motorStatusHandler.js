f82201b943c4c236161017b5630ef6d3131e4266
logan hedin <6.28x10e18@gmail.com>
2023-11-18 11:18:24 +0000

    updating of the text content of the status content labeling

--- static/js/motorStatusHandler.js
+++ static/js/motorStatusHandler.js
@@ -4,9 +4,17 @@ var socket = io.connect('http://' + document.domain + ':' + location.port);
 
 socket.on('motor_status_updated', function(data) {
     console.log('Motor Status Update Received:', data);
+
     // Update the UI based on the received data
+    // Update the text content of the status label
     var statusElement = document.getElementById('status_label_' + data.motor_switch_id);
     if (statusElement) {
         statusElement.textContent = data.status;
+    }
+
+    // Update the checkbox state
+    var switchElement = document.getElementById(data.motor_switch_id + '-switch');
+    if (switchElement) {
+        switchElement.checked = (data.status === 'Active'); // Update the checkbox based on the motor status
     }
 });
