9fb5a7311bd7f4f67077bd0d1addcfa055e7dcd6
logan hedin <6.28x10e18@gmail.com>
2023-11-20 02:01:52 +0000

    changed all db classes to incorperate pymysql

--- modules/db.py
+++ modules/db.py
@@ -1,7 +1,5 @@
-# /modules/db.py
-
-import mysql.connector
 import logging
+import pymysql
 from config import DB_CONFIG
 from datetime import datetime
 
@@ -11,10 +9,10 @@ class Database:
     def __init__(self):
         """Initialize database connection."""
         try:
-            self.connection = mysql.connector.connect(**DB_CONFIG)
-            self.cursor = self.connection.cursor(dictionary=True)
+            self.connection = pymysql.connect(**DB_CONFIG)
+            self.cursor = self.connection.cursor(pymysql.cursors.DictCursor)
             logger.info("Database connection established.")
-        except mysql.connector.Error as err:
+        except pymysql.MySQLError as err:
             logger.error(f"Error connecting to the database: {err}")
             raise
 
@@ -35,7 +33,7 @@ class Database:
             if self.connection:
                 self.connection.close()
                 logger.info("Database connection closed.")
-        except mysql.connector.Error as err:
+        except pymysql.MySQLError as err:
             logger.error(f"Error closing database resources: {err}")
 
     def update_time_settings(self, roll_up_time, roll_down_time):
@@ -50,7 +48,7 @@ class Database:
             self.cursor.execute(query, values)
             self.connection.commit()
             logger.info("Time settings updated successfully.")
-        except mysql.connector.Error as err:
+        except pymysql.MySQLError as err:
             logger.error(f"Error updating time settings: {err}")
 
     def get_latest_time_settings(self):
@@ -59,19 +57,8 @@ class Database:
             query = "SELECT roll_up_time, roll_down_time FROM time_settings ORDER BY id DESC LIMIT 1"
             self.cursor.execute(query)
             return self.cursor.fetchone()
-        except mysql.connector.Error as err:
+        except pymysql.MySQLError as err:
             logger.error(f"Error retrieving latest time settings: {err}")
-            return None
-
-    def get_time_setting(self, setting_name):
-        """Retrieve a specific time setting (roll_up_time or roll_down_time)."""
-        try:
-            sql = f"SELECT {setting_name} FROM time_settings ORDER BY id DESC LIMIT 1"
-            self.cursor.execute(sql)
-            result = self.cursor.fetchone()
-            return result[setting_name] if result else None
-        except mysql.connector.Error as err:
-            logger.error(f"Error retrieving {setting_name}: {err}")
             return None
 
     def get_motor_statuses(self):
@@ -80,7 +67,7 @@ class Database:
             query = "SELECT motor_id, status FROM motor_statuses"
             self.cursor.execute(query)
             return self.cursor.fetchall()
-        except mysql.connector.Error as err:
+        except pymysql.MySQLError as err:
             logger.error(f"Error retrieving motor statuses: {err}")
             return []
 
@@ -92,30 +79,8 @@ class Database:
             self.cursor.execute(query, values)
             self.connection.commit()
             logger.info(f"Motor status for motor_id={motor_id} updated successfully to {status}.")
-        except mysql.connector.Error as err:
+        except pymysql.MySQLError as err:
             logger.error(f"Error updating motor status for motor_id={motor_id}: {err}")
-
-    def save_api_token(self, token, expiry_time):
-        """Save the API token and its expiry time."""
-        try:
-            query = "REPLACE INTO api_tokens (token, expiry_time) VALUES (%s, %s)"
-            values = (token, expiry_time)
-            self.cursor.execute(query, values)
-            self.connection.commit()
-            logger.info("API token saved successfully.")
-        except mysql.connector.Error as err:
-            logger.error(f"Error saving API token: {err}")
-
-    def get_api_token(self):
-        """Retrieve the API token and its expiry time."""
-        try:
-            query = "SELECT token, expiry_time FROM api_tokens LIMIT 1"
-            self.cursor.execute(query)
-            result = self.cursor.fetchone()
-            return result if result else (None, None)
-        except mysql.connector.Error as err:
-            logger.error(f"Error retrieving API token: {err}")
-            return None, None
 
     def save_sensor_data(self, timestamp, temperature, humidity, vpd):
         """Save sensor data (temperature, humidity, VPD) to the database."""
@@ -128,9 +93,11 @@ class Database:
             self.cursor.execute(query, values)
             self.connection.commit()
             logger.info("Sensor data saved successfully.")
-        except mysql.connector.Error as err:
+        except pymysql.MySQLError as err:
             logger.error(f"Error saving sensor data (Temperature: {temperature}, Humidity: {humidity}, VPD: {vpd}): {err}")
         except Exception as e:
             logger.error(f"Unexpected error in save_sensor_data (Temperature: {temperature}, Humidity: {humidity}, VPD: {vpd}): {e}")
 
-# Additional methods here
+    # Note: Other methods specific to the application here, 
+
+# End of the Database class
